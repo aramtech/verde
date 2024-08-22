1625f4d2a3789cfc1271220470047edbbe146536
mse99 <mohamedsalemedrah@gmail.com>
2024-08-22 17:26:05 +0200

    Remove awaits on sync fs calls in `fs.ts`

--- src/fs.ts
+++ src/fs.ts
@@ -16,11 +16,11 @@ export type CollectResult = {
 export const fileExists = (p: string) => fs.existsSync(p);
 
 export const collectFilePathsIn = async (dir: string) => {
-    const contents = await fs.readdirSync(dir);
+    const contents = fs.readdirSync(dir);
     let results: string[] = [];
 
     for (const c of contents) {
-        const stats = await fs.statSync(path.join(dir, c));
+        const stats = fs.statSync(path.join(dir, c));
 
         if (stats.isDirectory()) {
             const gotten = await collectFilePathsIn(path.join(dir, c));
@@ -42,13 +42,13 @@ export const collectDirsWithFile = async (initialPath: string, opts: CollectOpts
         return [];
     }
 
-    const pathStats = await fs.statSync(initialPath);
+    const pathStats = fs.statSync(initialPath);
 
     if (!pathStats.isDirectory()) {
         return [];
     }
 
-    const contents = await fs.readdirSync(initialPath);
+    const contents = fs.readdirSync(initialPath);
 
     if (contents.find(x => x === configFilename)) {
         const fullContents = await collectFilePathsIn(initialPath);
@@ -59,7 +59,7 @@ export const collectDirsWithFile = async (initialPath: string, opts: CollectOpts
 
     for (const c of contents) {
         const fullPath = path.join(initialPath, c);
-        const stats = await fs.statSync(fullPath);
+        const stats = fs.statSync(fullPath);
 
         if (stats.isDirectory()) {
             const gotten = await collectDirsWithFile(fullPath, opts);
@@ -73,12 +73,12 @@ export const collectDirsWithFile = async (initialPath: string, opts: CollectOpts
 export const readFiles = async (paths: string[]) => await Promise.all(paths.map(f => fs.readFileSync(f)));
 
 export const storeObjectInCwd = async <T>(nameOrPath: string, object: T) =>
-    await fs.writeFileSync(nameOrPath, JSON.stringify(object));
+    fs.writeFileSync(nameOrPath, JSON.stringify(object));
 
-export const isStoredOnDisk = async (nameOrPath: string) => await fs.existsSync(nameOrPath);
+export const isStoredOnDisk = async (nameOrPath: string) => fileExists(nameOrPath);
 
 export const readJSON = async <T>(path: string) => {
-    const contents = await fs.readFileSync(path);
+    const contents = fs.readFileSync(path);
     return JSON.parse(contents.toString("utf-8")) as T;
 };
 
@@ -91,7 +91,7 @@ export const removeDir = async (p: string) => fs.rmdirSync(p, { recursive: true 
 export async function find_project_root(currentDir = path.resolve(".")) {
     const packagePath = path.join(currentDir, "package.json");
 
-    if (await fs.existsSync(packagePath)) {
+    if (fileExists(packagePath)) {
         return currentDir;
     }
 
