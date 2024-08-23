484201a84614cf23600961340bcee5d5c7349c25
mse99 <mohamedsalemedrah@gmail.com>
2024-08-23 21:14:52 +0200

    Move `reveal` commands test to `reveal.test.ts`

--- tests/commands.test.ts
+++ tests/commands.test.ts
@@ -32,48 +32,6 @@ describe("CLI", () => {
         return name;
     };
 
-    test("hide command: no matching utility found.", async () => {
-        vi.spyOn(console, "error");
-
-        const testDirPath = await moveToTestDir();
-
-        await storeObjectInCwd("package.json", { name: "FOO" });
-
-        await fs.mkdir(path.join(testDirPath, "foo-util"));
-        await fs.writeFile(
-            path.join(testDirPath, "foo-util", "utils.json"),
-            JSON.stringify({ name: "foo", deps: {}, version: "10.0.0", hash: "foo" }),
-        );
-
-        const cmd = addCommands(new Command());
-        await cmd.parseAsync(["node", "verde", "hide", "baz"]);
-
-        expect(console.error).toHaveBeenCalledWith("could not find utility with name baz");
-    });
-
-    test("hide command: should update the config file of the utility.", async () => {
-        vi.spyOn(console, "error");
-
-        const testDirPath = await moveToTestDir();
-
-        await storeObjectInCwd("package.json", { name: "FOO" });
-
-        await fs.mkdir(path.join(testDirPath, "foo-util"));
-        await fs.writeFile(
-            path.join(testDirPath, "foo-util", "utils.json"),
-            JSON.stringify({ name: "foo", deps: {}, version: "10.0.0", hash: "foo" }),
-        );
-
-        const cmd = addCommands(new Command());
-        await cmd.parseAsync(["node", "verde", "hide", "foo"]);
-
-        expect(console.error).not.toHaveBeenCalledWith("could not find utility with name foo");
-
-        const utilFile = await readJSON<UtilityFile>("./foo-util/utils.json");
-
-        expect(utilFile.private).toBe(true);
-    });
-
     test("reveal command: no matching utility found.", async () => {
         vi.spyOn(console, "error");
 
