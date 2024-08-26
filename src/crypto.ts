93cdb68773bdd175ee3e79290e38ea93513d2c8e
mse99 <mohamedsalemedrah@gmail.com>
2024-08-26 18:59:19 +0200

    Add encryption helpers to `src/crypto.ts`

--- src/crypto.ts
+++ src/crypto.ts
@@ -1,4 +1,25 @@
-import { createHash } from "crypto";
+import crypto from "crypto";
 
 export const hashBuffersWithSha256 = (buffers: Buffer[]): string =>
-    buffers.reduce((acc, item) => acc.update(item), createHash("sha256")).digest("base64url");
+    buffers.reduce((acc, item) => acc.update(item), crypto.createHash("sha256")).digest("base64url");
+
+const hashStringWithSha256 = (str: string) => crypto.createHash("sha256").update(str).digest("base64");
+
+export const encryptBufferWithPassword = (buff: Buffer, password: string) => {
+    const passwordHash = hashStringWithSha256(password);
+    const passwordBytes = Buffer.from(passwordHash.slice(0, 32));
+
+    const cipher = crypto.createCipheriv("aes256", passwordBytes, passwordBytes.subarray(0, 16));
+    cipher.update(buff);
+    return cipher.final();
+};
+
+export const decryptBufferWithPassword = (buff: Buffer, password: string) => {
+    const passwordHash = hashStringWithSha256(password);
+    const passwordBytes = Buffer.from(passwordHash.slice(0, 32));
+
+    const decipher = crypto.createDecipheriv("aes256", passwordBytes, passwordBytes.subarray(0, 16));
+    decipher.update(buff);
+
+    return decipher.final();
+};
