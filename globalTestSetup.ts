dcb627145a81f7561e5e8be5f914abe5a37f825e
mse99 <mohamedsalemedrah@gmail.com>
2024-08-29 09:14:39 +0200

    Stop using actual homedir in tests

--- globalTestSetup.ts
+++ globalTestSetup.ts
@@ -2,6 +2,4 @@ import fs from "fs-extra";
 import path from "path";
 import os from "os";
 
-if (!fs.existsSync(path.join(os.homedir(), ".verde"))) {
-    await fs.mkdirSync(path.join(os.homedir(), ".verde"));
-}
+fs.mkdirpSync(path.join(os.tmpdir(), "verde", ".verde"));
