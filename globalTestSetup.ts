a81b5227a59e29cd2245c5c129b4542f0a8be350
mse99 <mohamedsalemedrah@gmail.com>
2024-08-29 10:17:32 +0200

    Add a an additional test to `pull.test.ts`

--- globalTestSetup.ts
+++ globalTestSetup.ts
@@ -2,4 +2,8 @@ import fs from "fs-extra";
 import path from "path";
 import os from "os";
 
+if (fs.existsSync(path.join(os.tmpdir(), "verde"))) {
+    fs.removeSync(path.join(os.tmpdir(), "verde"));
+}
+
 fs.mkdirpSync(path.join(os.tmpdir(), "verde", ".verde"));
--- tests/pull.test.ts
+++ tests/pull.test.ts
@@ -5,6 +5,9 @@ import fs from "node:fs/promises";
 import { randomInt } from "crypto";
 import { storeJSON } from "../src/fs";
 
+import * as Prompt from "../src/prompt";
+import { encryptAndSaveFileToStorage } from "../src/storage/encrypted";
+
 describe("pull", () => {
     let originalCwd: string = process.cwd();
 
@@ -40,5 +43,29 @@ describe("pull", () => {
 
         expect(process.exit).toHaveBeenCalledWith(1);
         expect(console.error).toHaveBeenCalledWith("foo.bar.baz is not a valid version");
+    });
+
+    test("prompt the user for the tokens if no tokens exist and print an error and exit if the maximum try count exceeds.", async () => {
+        vi.spyOn(console, "error");
+        vi.spyOn(process, "exit");
+
+        vi.spyOn(Prompt, "readAnswerTo")
+            .mockResolvedValue("aramtech-is-cool")
+            .mockResolvedValue("github-token-is-cool");
+
+        await moveToTestDir();
+
+        await storeJSON("package.json", { name: "foo", version: "1.0.0" });
+
+        try {
+            const cmd = addCommands(new Command());
+            await cmd.parseAsync(["node", "verde", "pull", "common2"]);
+        } catch {}
+
+        expect(Prompt.readAnswerTo).toHaveBeenCalledWith("Please input your organization name:");
+        expect(Prompt.readAnswerTo).toHaveBeenCalledWith(
+            "Please provide your classic personal github access token (you can create one at https://github.com/settings/tokens)\n\n Token:",
+        );
+        expect(console.error).toHaveBeenCalledWith("Maximum try count exceeded");
     });
 });
